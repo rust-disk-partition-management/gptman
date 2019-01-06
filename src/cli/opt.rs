295f43188ced666a9c6ca55259474ff7aba0dfe8
Cecile Tonglet <cecile.tonglet@cecton.com>
2019-01-06 12:55:54 +0100

    Fixed: set default columns

--- src/cli/opt.rs
+++ src/cli/opt.rs
@@ -27,6 +27,7 @@ pub struct Opt {
     #[structopt(
         short = "o",
         long = "output",
+        default_value = "Device,Start,End,Sectors,Size,Type,GUID,Attributes,Name",
         raw(use_delimiter = "true", possible_values = "&Column::variants()")
     )]
     pub columns: Vec<Column>,
