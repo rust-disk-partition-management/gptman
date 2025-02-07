52d7e0f00b82607c951da7c112f2c5bb41f4c774
eatradish <sakiiily@aosc.io>
2025-02-07 21:38:28 +0800

    fix!: fix get sector size on mips64

--- src/linux.rs
+++ src/linux.rs
@@ -5,9 +5,13 @@ use std::os::unix::io::AsRawFd;
 use thiserror::Error;
 
 mod ioctl {
-    use nix::{ioctl_none, ioctl_read_bad};
+    use nix::{ioctl_none, ioctl_read_bad, request_code_none};
 
-    ioctl_read_bad!(blksszget, 0x1268, u64);
+    ioctl_read_bad!(
+        blksszget,
+        request_code_none!(0x12, 104),
+        ::std::os::raw::c_int
+    );
     ioctl_none!(blkrrpart, 0x12, 95);
 }
 
@@ -50,7 +54,7 @@ pub fn reread_partition_table(file: &mut fs::File) -> Result<(), BlockError> {
 }
 
 /// Makes an ioctl call to obtain the sector size of a block device
-pub fn get_sector_size(file: &mut fs::File) -> Result<u64, BlockError> {
+pub fn get_sector_size(file: &mut fs::File) -> Result<i32, BlockError> {
     let metadata = file.metadata().map_err(BlockError::Metadata)?;
     let mut sector_size = 512;
 
